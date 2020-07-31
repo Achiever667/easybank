41f98b018dd14db00ede1c7ca04e00838444b864
kelechioleh@gmail.com <achiever667>
2020-07-31 11:59:17 +0100

    finall result

--- script.js
+++ script.js
@@ -4,14 +4,12 @@ var navbarTogglerIcon = document.getElementById('togglerIcon');
 navbarTogglerIcon === 'clicked';
 navbarTogglerIcon.addEventListener('click', function myFunction() {
     if( navbarTogglerIcon += 'clicked'){
-     document.getElementById('second-top0').style.background = 'linear-gradient(to bottom, rgb(209, 207, 207), rgb(209, 207, 207))';
     var element = document.getElementById("navbarTogglerDemo02");
     element.classList.toggle("mystyle");
     }
     else{
         top.style.background = 'white';
-        var element = document.getElementById("navbarTogglerDemo02");
-        element.classList.toggle("mystyle");
+       
       
     }
  });
